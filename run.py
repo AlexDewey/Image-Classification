2db3947b0e30de50a918acf681edfa2e0f08a621
Alex Dewey <alexmarcdewey@gmail.com>
2022-09-10 18:26:45 -0700

    do_splits refactoring

--- run.py
+++ run.py
@@ -15,13 +15,26 @@ def refresh_dir(path, name):
         os.mkdir(path + name)
 
 
-def do_splits(directory_given):
+def do_splits(src, directory_given):
     # 70, 15, 15 split
     allFileNames = os.listdir(images_path + directory_given)
     np.random.shuffle(allFileNames)
     train, test, validate = np.split(np.array(allFileNames),
                                      [int(len(allFileNames) * 0.7), int(len(allFileNames) * 0.85)])
-    return [train, test, validate]
+    train_FileNames = [src + directory_given + "\\" + name for name in train.tolist()]
+    val_FileNames = [src + directory_given + "\\" + name for name in validate.tolist()]
+    test_FileNames = [src + directory_given + "\\" + name for name in test.tolist()]
+
+    # Copy-pasting images
+    for name in train_FileNames:
+        shutil.copy(name, src + '/train//' + cls)
+
+    for name in val_FileNames:
+        shutil.copy(name, src + '/val//' + cls)
+
+    for name in test_FileNames:
+        shutil.copy(name, src + '/test//' + cls)
+    return [train_FileNames, test_FileNames, val_FileNames]
 
 
 refresh_dir(images_path, "\Bunnies-Train")
@@ -31,29 +44,12 @@ refresh_dir(images_path, "\Other-Train")
 refresh_dir(images_path, "\Other-Validate")
 refresh_dir(images_path, "\Other-Test")
 
-# bunny, 611
-bunny_train, bunny_test, bunny_validate = do_splits("\Bunnies-Base")
-# human, 100
-human_train, human_test, human_validate = do_splits("\Other-Base\\Human")
-# nature background, 100
-nature_train, nature_test, nature_validate = do_splits("\Other-Base\\Nature-Background")
-# text, 75
-text_train, text_test, text_validate = do_splits("\Other-Base\\Text")
-# dogs, 50
-dogs_train, dogs_text, dogs_validate = do_splits("\Other-Base\\Dogs")
-# cats, 50
-cats_train, cats_text, cats_validate = do_splits("\Other-Base\\Cats")
-# hamsters, 50
-hamsters_train, hamsters_text, hamsters_validate = do_splits("\Other-Base\\Hamsters")
-# suggestively sexual, 50
-ss_train, ss_text, ss_validate = do_splits("\Other-Base\\Suggestively-Sexual")
-# suggestively violent, 50
-sv_train, sv_text, sv_validate = do_splits("\Other-Base\\Suggestively-Violent")
-# abstract background, 50
-ab_train, ab_text, ab_validate = do_splits("\Other-Base\\Abstract-Background")
-# empty cages, 25
-ec_train, ec_text, ec_validate = do_splits("\Other-Base\\Empty-Cages")
-# bunny drawings 11
-bd_train, bd_text, bd_validate = do_splits("\Other-Base\\Bunny-Drawings")
+classes = ["\Bunnies-Base", "\Other-Base\\Human", "\Other-Base\\Nature-Background", "\Other-Base\\Text",
+           "\Other-Base\\Dogs", "\Other-Base\\Cats", "\Other-Base\\Hamsters", "\Other-Base\\Suggestively-Sexual",
+           "\Other-Base\\Suggestively-Violent", "\Other-Base\\Abstract-Background", "\Other-Base\\Empty-Cages",
+           "\Other-Base\\Bunny-Drawings"]
+
+for class_dir in classes:
+    do_splits(images_path, class_dir)
 
 print("test")
